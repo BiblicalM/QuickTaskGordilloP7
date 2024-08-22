263d945c435af666b24b7be82540094c41cbdd8b
Maximiliano Gordillo <724910@student.dusd.net>
2024-08-22 15:28:31 -0700

    added jump
    BUT IT DON'T WORK X(

--- QuickTaskGordillo/Assets/Scripts/PlayerController.cs
+++ QuickTaskGordillo/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private float horizontalMovement;
+    private Rigidbody2D rb;
+
     public float speed = 8;
+    private float jumpForce = 14;
+    public GameObject groundCheck;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         horizontalMovement = Input.GetAxis("Horizontal");
+        transform.Translate(UnityEngine.Vector2.right * horizontalMovement * speed * Time.deltaTime);
+        //rb.velocity = UnityEngine.Vector2.right * horizontalMovement * speed * Time.deltaTime;
+        //rb.velocity.Set(horizontalMovement * speed * Time.deltaTime, 0);
 
-        transform.Translate(Vector2.right * horizontalMovement * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new UnityEngine.Vector2(0, jumpForce) * Time.deltaTime, ForceMode2D.Impulse);
+        }
     }
+
 }
